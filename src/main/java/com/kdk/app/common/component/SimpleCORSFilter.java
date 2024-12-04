2837471cc937fc00be3b5d83c493b96b6122f482
kdk1026 <kdk1026@gmail.com>
2024-12-04 19:55:52 +0900

    CORS 관련 수정 및 추가

---
+++ src/main/java/com/kdk/app/common/component/SimpleCORSFilter.java
@@ -0,0 +1,60 @@
+package com.kdk.app.common.component;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * <pre>
+ * -----------------------------------
+ * 개정이력
+ * -----------------------------------
+ * 2024. 12. 4. kdk	최초작성
+ * </pre>
+ *
+ * <pre>
+ * FrontEnd 에서 PUT 요청 시, OPTIOS 401 에러 발생 처리
+ *   - 아마 DELETE도 해당될 듯
+ * </pre>
+ *
+ * @author kdk
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ( "OPTIONS".equalsIgnoreCase(request.getMethod()) ) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
